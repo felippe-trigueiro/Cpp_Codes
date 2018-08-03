9ddf5f6b5c3a47e5563c7f0895b6254c0a94f49c
Felippe Trigueiro Angelo <trigueiro.angelo@outlook.com>
2018-08-03 01:49:20 -0300

    Insertion of the time measuring.

--- max_pairwise_product/max_pairwise_product/algorithm_pair.cpp
+++ max_pairwise_product/max_pairwise_product/algorithm_pair.cpp
@@ -21,3 +21,16 @@ long long MaxPairwiseProduct(const vector<int>& numbers) {
 	result = maior[0] * seg_maior;
 	return result;
 }
+
+long long MaxPairwiseProduct_Naive(const vector<int>& numbers) 
+{
+	long long result = 0;
+	int n = numbers.size();
+	
+	for (int i = 0; i < n; i++)
+		for (int j = i+1; j < n; j++)
+			if ((numbers[i] * numbers[j] > result))
+				result = long long (numbers[i]) * long long (numbers[j]);
+
+	return result;
+}
--- max_pairwise_product/max_pairwise_product/main.cpp
+++ max_pairwise_product/max_pairwise_product/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 using std::vector;
 using std::cin;
 using std::cout;
 
 long long MaxPairwiseProduct(const vector<int>&);
+long long MaxPairwiseProduct_Naive(const vector<int>&);
 
 int main() {
 	int n;
@@ -14,9 +16,26 @@ int main() {
 	for (int i = 0; i < n; ++i) {
 		cin >> numbers[i];
 	}
+	
+	//Measuring the time of the fast algorithm
+	auto start = std::chrono::high_resolution_clock::now();
+	long long result_fast = MaxPairwiseProduct(numbers);
+	auto finish = std::chrono::high_resolution_clock::now();
 
-	long long result = MaxPairwiseProduct(numbers);
-	cout << result << "\n";
+	std::chrono::duration<double> elapsed_fast = finish - start;
+
+	//Measuring the time of the naive algorithm
+	auto start_naive = std::chrono::high_resolution_clock::now();
+	long long result_naive = MaxPairwiseProduct_Naive(numbers);
+	auto finish_naive = std::chrono::high_resolution_clock::now();
+
+	std::chrono::duration<double> elapsed_naive = finish_naive - start_naive;
+
+	cout << "Fast result: " << result_fast << "\n";
+	cout << "Time of the fast algorithm " << elapsed_fast.count() << "\n\n";
+
+	cout << "Naive result: " << result_naive << "\n";
+	cout << "Time of the naive algorithm " << elapsed_naive.count() << "\n\n";
 	system("pause");
 	return 0;
 }
